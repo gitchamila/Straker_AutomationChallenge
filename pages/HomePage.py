1b5a6354e5240b6438b7d22ea99792e48b9f6584
Chamila Madusankha <chamilamadusankha@gmail.com>
2025-04-10 11:30:16 +1200

    Error Handling
    Add log and Screen shot

--- pages/HomePage.py
+++ pages/HomePage.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page
 from pages.BasePage import BasePage
 from playwright.sync_api import expect
+from utils.conftest import logger
 
 class HomePage(BasePage):
     def __init__(self, page: Page):
@@ -11,6 +12,7 @@ class HomePage(BasePage):
     def navigate_straker(self):
         try:
             self.page.goto("/",timeout=10000)
+            logger.info("Homepage loaded successfully.")
         except TimeoutError:
             raise Exception("Failed to load the homepage within the timeout.")
 
@@ -23,8 +25,11 @@ class HomePage(BasePage):
 
             expect(self.page.locator("#w-dropdown-list-5")).to_contain_text("tiri")
             expect(self.learn_more_button).to_be_visible()
-
+            logger.info("Clicking the 'Learn More' button.")
             self.learn_more_button.click()
 
-        except TimeoutError:
-            raise Exception("Failed to locate 'Learn more' under Products")
+        except Exception as e:
+            logger.error(f"Error occurred: {str(e)}")
+            self.page.take_screenshot_on_failure(test_name="Learn_more_failure")  # Capture screenshot on failure
+            raise Exception("Failed to locate or interact with 'Learn more' under Products")
+
